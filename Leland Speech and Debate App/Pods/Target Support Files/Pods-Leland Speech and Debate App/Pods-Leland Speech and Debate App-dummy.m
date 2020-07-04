3c0b07a28c42914c957a14af8af17129e1e79e8b
nikhilChandra1 <nikhil.chandra2021@gmail.com>
2020-07-03 20:21:32 -0700

    All code so fare

---
+++ Leland Speech and Debate App/Pods/Target Support Files/Pods-Leland Speech and Debate App/Pods-Leland Speech and Debate App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Leland_Speech_and_Debate_App : NSObject
+@end
+@implementation PodsDummy_Pods_Leland_Speech_and_Debate_App
+@end
---
+++ Leland Speech and Debate App/Pods/Target Support Files/Pods-Leland Speech and Debate AppUITests/Pods-Leland Speech and Debate AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Leland_Speech_and_Debate_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Leland_Speech_and_Debate_AppUITests
+@end
