2f64e4152b873388532c4748b8ace947cf5abea9
nikhilChandra1 <nikhil.chandra2021@gmail.com>
2020-07-26 16:20:35 -0700

    m

---
+++ Leland Speech and Debate App/Leland Speech and Debate App/Demo Cell.swift
@@ -0,0 +1,39 @@
+//
+//  DemoCell.swift
+//  FoldingCell
+//
+//  Created by Alex K. on 25/12/15.
+//  Copyright © 2015 Alex K. All rights reserved.
+//
+
+import FoldingCell
+import UIKit
+
+class DemoCell: FoldingCell {
+
+    @IBOutlet weak var eventLabel: UILabel!
+    @IBOutlet weak var eventImage: UIImageView!
+    
+
+    var number: Int = 0
+
+    override func awakeFromNib() {
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.layer.masksToBounds = true
+        super.awakeFromNib()
+    }
+
+    override func animationDuration(_ itemIndex: NSInteger, type _: FoldingCell.AnimationType) -> TimeInterval {
+        let durations = [0.26, 0.2, 0.2]
+        return durations[itemIndex]
+    }
+}
+
+// MARK: - Actions ⚡️
+
+extension DemoCell {
+
+    @IBAction func buttonHandler(_: AnyObject) {
+        print("tap")
+    }
+}
